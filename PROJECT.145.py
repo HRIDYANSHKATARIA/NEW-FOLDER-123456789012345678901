bd54fe36ad761f459dba29d434081d3b5aecc9be
HRIDYANSHKATARIA <85940668+HRIDYANSHKATARIA@users.noreply.github.com>
2022-05-31 13:12:15 +0530

    Add files via upload

---
+++ PROJECT.145.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Driving License")
+root.geometry("400x400")
+
+print("Driving License")
+print("ID : 281764972661")
+print("NAME: HRIDYANSH KATARIA")
+print("DATE OF BIRTH: 21|12|2010")
+print("PIN NUMBER: 674239")
+print("ADDRESS: DELHI, INDIA")
+print("AUTHORISATION TO DRIVE THE FOLLOWING VEHICLES = 2 WHEELER AND 4 WHEELER")
